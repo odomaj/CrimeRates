3e4b593198d6ef2f719e533ec62580aa1027596a
odomaj <odomaj@clarkson.edu>
2023-11-29 20:55:44 -0500

    commented code

--- main.R
+++ main.R
@@ -7,6 +7,8 @@ rm(list = ls())
 # retrieve data from the scv file
 all_data <- read.csv("communities.csv")
 
+# perform multilinear regression
 fit_mlr <- lm(ViolentCrimesPerPop ~ PopDens + medIncome + PctUnemployed, data = all_data)
 
+# print the summary of regression model
 print(summary(fit_mlr))
