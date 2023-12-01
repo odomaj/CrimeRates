857c95d2bee1ccb8a936827a5a69452473423ec9
odomaj <odomaj@clarkson.edu>
2023-11-30 19:31:00 -0500

    Added VIF values and graphs

--- main.R
+++ main.R
@@ -1,3 +1,5 @@
+library(car)
+
 # clean the console
 cat("\f")
 
@@ -12,3 +14,13 @@ fit_mlr <- lm(ViolentCrimesPerPop ~ PopDens + medIncome + PctUnemployed, data = 
 
 # print the summary of regression model
 print(summary(fit_mlr))
+
+# calculate VIF Values
+vif_values <- vif(fit_mlr)
+print("VIF Values")
+print(vif_values)
+
+# generate graphs
+plot(all_data$PopDens, all_data$ViolentCrimesPerPop, main = "ViolentCrimesPerPop v. PopDens", xlab = "Normalized Population Density", ylab = "Normalized Violent Crimes per 100k People", pch = 20)
+plot(all_data$medIncome, all_data$ViolentCrimesPerPop, main = "ViolentCrimesPerPop v. medIncome", xlab = "Normalized Median Income", ylab = "Normalized Violent Crimes per 100k People", pch = 20)
+plot(all_data$PctUnemployed, all_data$ViolentCrimesPerPop, main = "ViolentCrimesPerPop v. PctUnemployed", xlab = "Normalized Unemployment Rate", ylab = "Normalized Violent Crimes per 100k People", pch = 20)
